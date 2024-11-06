ce0991a2fb6f3a742bff272ad5d8073e0df1997a
VikaFajarAshari24 <vikafajarashari24@gmail.com>
2024-11-06 21:13:48 +0700

    fix: setting the logic for RPS game

--- app.js
+++ app.js
@@ -7,34 +7,77 @@ const { Server } = require("socket.io");
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: "http://localhost:5174",
+    origin: "http://localhost:5173",
   },
 });
 
 let users = [];
-let RPS = [];
 
 io.on("connection", (socket) => {
-  console.log(`User is connected with id ${socket.id}`);
+  console.log(`User connected with id ${socket.id}`);
 
   socket.on("joinGame", (username) => {
     users.push({
       id: socket.id,
       username,
+      points: 0,
     });
     io.emit("welcomingUser", {
-      message: `Welcome to this RPS Game, ${username}`,
+      message: `Welcome to the RPS Game, ${username}`,
       users,
     });
+
+    if (users.length === 2) {
+      io.emit("startGame", { message: "Game is starting!" });
+    }
+  });
+
+  socket.on("makeChoice", ({ userChoice, username }) => {
+    const opponent = users.find((user) => user.id !== socket.id);
+    const opponentChoice = ["rock", "paper", "scissors"][Math.floor(Math.random() * 3)];
+
+    let resultMessage = `${username} chose ${userChoice}. Opponent chose ${opponentChoice}. `;
+
+    if (userChoice === opponentChoice) {
+      users = users.map((user) => {
+        user.points += 1;
+        return user;
+      });
+      resultMessage += "It's a tie! Both gain 1 point.";
+    } else if (
+      (userChoice === "rock" && opponentChoice === "scissors") ||
+      (userChoice === "scissors" && opponentChoice === "paper") ||
+      (userChoice === "paper" && opponentChoice === "rock")
+    ) {
+      users = users.map((user) => {
+        if (user.id === socket.id) user.points += 3;
+        return user;
+      });
+      resultMessage += `${username} wins this round! Gains 3 points.`;
+    } else {
+      users = users.map((user) => {
+        if (user.id === opponent.id) user.points += 3;
+        return user;
+      });
+      resultMessage += `Opponent wins this round and gains 3 points.`;
+    }
+
+    io.emit("gameResult", {
+      users,
+      message: resultMessage,
+    });
+
+    
+    const userWithMaxPoints = users.find((user) => user.points >= 10);
+    if (userWithMaxPoints) {
+      io.emit("gameOver", { message: `${userWithMaxPoints.username} wins the game!` });
+      users = [];
+    }
   });
 
   socket.on("disconnect", () => {
-    console.log(`User disconnected with id ${socket.id}`);
-    users = users.filter((user) => {
-      return user.id !== socket.id;
-    });
-    console.log(users, "<-- users left");
-    io.emit("Users remaining", users);
+    users = users.filter((user) => user.id !== socket.id);
+    io.emit("resetGame", { message: "A player has left. The game has been reset." });
   });
 });
 

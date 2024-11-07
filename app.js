75409a2367cd94c22e01b8201dcb99e8153d3ad8
YuliaRizki <kikiyulia223@gmail.com>
2024-11-07 13:55:50 +0700

    feat: Revising cors origin at app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
-  cors: { origin: "http://localhost:5173" },
+  cors: { origin: "*" },
 });
 
 let rooms = {};
